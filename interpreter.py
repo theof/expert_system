bf56746fc71610d3c4d1036c7a2aa99b38fc0845
Timothe LEBRIZE <tlebrize@e2r9p5.42.fr>
2017-01-24 22:27:23 +0100

    ayyyyy

--- interpreter.py
+++ interpreter.py
@@ -1,3 +1,5 @@
+MAX_DEPTH = 50
+
 class NodeVisitor(object):
 
 	def visit(self, node):
@@ -48,12 +50,14 @@ class Interpreter(NodeVisitor):
 	def visit_ParenExpr(self, node):
 		# print('visit_ParenExpr')
 		if isinstance(node.left, str):
+			self.dependences += node.left
 			return node.left in self.true and node.left not in self.false
 		else:
 			return self.visit(node.left)
 
 	def visit_AndConclusion(self, node):
 		# print('visit_AndConclusion')
+		self.updates = node.right
 		if node.negation:
 			self.false.append(node.right)
 		else:
@@ -66,15 +70,42 @@ class Interpreter(NodeVisitor):
 			self.visit(node.right)
 
 	def visit_Rule(self, node):
-		if self.visit(node.expression):
-			return self.visit(node.conclusion)
+		self.dependences = ''
+		self.outcomes = ''
+		self.visit(node.expression)
+		self.visit(node.conclusion)
+		return [self.dependences, self.outcomes, node, True]
+
+	def eval(self, node):
+		lines = []
+		for rule in node.rules:
+			lines.append(self.visit(rule))
+		self.dependences = [x[0] for x in lines]
+		self.outcomes = [x[1] for x in lines]
+
+		self.true = []
+		self.false = []
+		for fact in node.initial_facts:
+			self.true.append(fact)
+		deepness = 0
+		while True in [x[3] for x in lines] and deepness < MAX_DEPTH:
+			deepness += 1
+			for line in lines:
+				if line[3] and all([True if fact not in self.outcomes else False for fact in line[0]]):
+					if self.visit(line[2].expression):
+						self.visit(line[2].conclusion)
+						line[3] = False
+						for fact in line[1]:
+							self.dependences.replace(fact, '')
+		for line in lines:
+			if line[3]:
+				if self.visit(line[2].expression):
+					self.visit(line[2].conclusion)
 
 	def visit_System(self, node):
 		# print('visit_System')
-		for fact in node.initial_facts:
-			self.true.append(fact)
-		for rule in node.rules:
-			self.visit(rule)
+		self.eval(node)
+
 		for query in node.queries:
 			if query in self.true and query not in self.false:
 				print(query, ': True')
