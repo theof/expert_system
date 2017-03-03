ca1ef7babb0f1c6c77b1e88bccd8ce104bceafba
Theophile VALLEE <tvallee@student.42.fr>
2017-03-03 18:49:38 +0100

    This is the last time we let Michael commit ascii porn in the comments

--- src/parser.py
+++ src/parser.py
@@ -3,7 +3,8 @@ import fileinput
 def es_parse(line):
     line = line.split('#')[0]
     line = ''.join(line.split())
-    return 3
+
+    return line
 
 for line in fileinput.input():
     print('out: {}'.format(es_parse(line)))
